f03dea472033843cfdf15c1a779e3754fd4db7a1
Pip Grylls <pipgryllsastro@gmail.com>
2022-06-16 11:54:47 +0100

    altering config

--- bin/build_me.sh
+++ bin/build_me.sh
@@ -29,7 +29,7 @@ bundle install
 bundle exec jekyll serve
 
 rm setup.md
-rm -r _site/ venv/ collections/ fig/ _includes/rsg/*-lesson/ slides/ _includes/
+rm -r _site/ venv/ collections/ fig/ _includes/rsg/*-lesson/ slides/ _includes/ submodules/
 find -f ./data \! -name "*.md" -depth 1 -delete
 rm assets/favicons/rsg/apple* assets/favicons/rsg/favicon* assets/favicons/rsg/mstile*
 
